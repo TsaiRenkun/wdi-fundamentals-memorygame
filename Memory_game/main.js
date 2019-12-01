8ed1ac37b943c1c20c402d98e3564f77b505d12d
Tsai Renkun <tsairenkun@Tsais-MacBook-Pro.local>
2019-12-02 03:05:11 +0800

    created game logic

--- Memory_game/main.js
+++ Memory_game/main.js
@@ -1,7 +1,24 @@
+alert('Hello, friends.')
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardThree);
+
+var numCards;
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+
+
+function checkForMatch(){
+if ((cardsInPlay[0]) === (cardsInPlay[1])) {
+	alert("you found a match!");
+	}	else{
+			console.log("sorry try again");
+	}
+}
+
+function flipCard(cardId){
+	console.log("User flipped"  + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch(cardsInPlay);
+}
+
+flipCard(0);
+flipCard(2);
